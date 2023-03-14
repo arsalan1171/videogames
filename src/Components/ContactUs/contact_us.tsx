eff4cc4d5e3cfd4727f579841d872b516d8aac0d
arsalan665@gmail.com <arsalan665@gmail.com>
2023-03-14 21:36:35 +0200

    contact us form added

--- src/Components/ContactUs/contact_us.tsx
+++ src/Components/ContactUs/contact_us.tsx
@@ -1,11 +1,57 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import '../VideoGameList/card/filter_card.css';
 import './contact_us.css';
 
 const ContactUs = () => {
 
+    let [name, setName] = useState<string>("");
+    let [email, setEmail] = useState<string>("");
+    let [msg, setMsg] = useState<string>("");
+
+    const submitRequest=()=>{
+
+    }
+
     return (
-        <div className='container'>
-            {/* //Contact us */}
-        </div>
+            <Container>
+                <Row>
+                    <Col><div className='filter-container'>
+                        <p>Get in touch...</p>
+                        <Form>
+                            <Row>
+                            <Col> <Form.Group className="mb-3" controlId="formName">
+                                <Form.Label>Name (contains)</Form.Label>
+                                <Form.Control className='apply-dark' type="text"
+                                    placeholder="Text (string)" onChange={(e) => setName(e.target.value)} />
+                            </Form.Group> </Col>
+                            <Col>
+                            <Form.Group className="mb-3" controlId="formScore">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control className='apply-dark' type="text"
+                                    placeholder="Enter rating" onChange={(e) => setEmail(e.target.value)} />
+                            </Form.Group>
+                            </Col>
+                            </Row>
+
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Example textarea</Form.Label>
+                                <Form.Control className='apply-dark' as="textarea" rows={3} />
+                            </Form.Group>
+                           
+                            <Button onClick={()=>alert("")} className='apply-dark'
+                                style={{ border: '1px solid' }} type="button">
+                                Submit
+                            </Button>
+                        </Form>
+                    </div> 
+                    </Col>
+                </Row>
+            </Container>
     );
 }
 
